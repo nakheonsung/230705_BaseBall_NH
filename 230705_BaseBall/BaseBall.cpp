9f8a4f7e48dd1a12ec5fcea10a861f75dd3d1462
nakheon.sung <zlekflepqlf@gmail.com>
2023-07-05 17:10:41 +0900

    [BaseBall] func fix same number

--- 230705_BaseBall/BaseBall.cpp
+++ 230705_BaseBall/BaseBall.cpp
@@ -18,5 +18,12 @@ public:
 				throw length_error("Must be Number.");
 			}
 		}
+
+		if (guessNumber[0] == guessNumber[1]
+			|| guessNumber[0] == guessNumber[2]
+			|| guessNumber[1] == guessNumber[2])
+		{
+			throw length_error("Must not have the same number.");
+		}
 	}
 };

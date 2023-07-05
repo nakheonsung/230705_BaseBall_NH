7329d84dbaa2597e223a729e43362ced9e4707c1
nakheon.sung <zlekflepqlf@gmail.com>
2023-07-05 17:29:51 +0900

    [TC] add 3Strike

--- Sample-Test1/BaseBall_test.cpp
+++ Sample-Test1/BaseBall_test.cpp
@@ -21,12 +21,18 @@ public:
 	}
 };
 
-
 TEST_F(BaseballFixture, ThrowExceptionWhenInvalid)
 {
-	GuessResult result = game.guess("123");
-
 	assertIllegalArgument("12");
 	assertIllegalArgument("12s");
 	assertIllegalArgument("121");
+}
+
+TEST_F(BaseballFixture, Solved3Strike)
+{
+	GuessResult result = game.guess("123");
+
+	EXPECT_TRUE(result.solved);
+	EXPECT_EQ(3, result.strikes);
+	EXPECT_EQ(0, result.balls);
 }

811e0200270e4b07f96d02bcc53d1b82807b195a
nakheon.sung <zlekflepqlf@gmail.com>
2023-07-05 17:01:28 +0900

    [Refac] BaseBall class

--- 230705_BaseBall/BaseBall.cpp
+++ 230705_BaseBall/BaseBall.cpp
@@ -6,6 +6,9 @@ class Baseball
 public:
 	void guess (const string& string)
 	{
-		throw length_error("Must be three letters.");
+		if (string.length() != 3)
+		{
+			throw length_error("Must be three letters.");
+		}
 	}
 };

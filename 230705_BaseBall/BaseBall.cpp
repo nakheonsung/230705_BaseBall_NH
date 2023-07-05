614c0de6c79d2acb93d69edd16babcd5339ac979
nakheon.sung <zlekflepqlf@gmail.com>
2023-07-05 17:07:47 +0900

    [Refac] func argument name

--- 230705_BaseBall/BaseBall.cpp
+++ 230705_BaseBall/BaseBall.cpp
@@ -4,14 +4,14 @@ using namespace std;
 class Baseball
 {
 public:
-	void guess (const string& string)
+	void guess (const string& guessNumber)
 	{
-		if (string.length() != 3)
+		if (guessNumber.length() != 3)
 		{
 			throw length_error("Must be three letters.");
 		}
 
-		for (char ch : string)
+		for (char ch : guessNumber)
 		{
 			if (ch < '0' || ch > '9')
 			{

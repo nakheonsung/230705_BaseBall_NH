149a7516ffe24f9322bbd06ad44706ba0121b0d3
nakheon.sung <zlekflepqlf@gmail.com>
2023-07-06 09:03:17 +0900

    [TC] add 1Strike 1Ball

--- Sample-Test1/BaseBall_test.cpp
+++ Sample-Test1/BaseBall_test.cpp
@@ -55,6 +55,15 @@ TEST_F(BaseballFixture, 1Strike2Ball)
 	EXPECT_EQ(2, result.balls);
 }
 
+TEST_F(BaseballFixture, 1Strike1Ball)
+{
+	GuessResult result = game.guess("325");
+
+	EXPECT_TRUE(!result.solved);
+	EXPECT_EQ(1, result.strikes);
+	EXPECT_EQ(1, result.balls);
+}
+
 TEST_F(BaseballFixture, 0Strike3Ball)
 {
 	GuessResult result = game.guess("231");
